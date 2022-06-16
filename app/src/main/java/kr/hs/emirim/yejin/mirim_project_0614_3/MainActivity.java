59d80b5b6260646c5b08056ec98798affbedf9d7
이 예진 <s2108@e-mirim.hs.kr>
2022-06-16 19:07:21 +0900

    직접 풀어보기 6-2 완성2

--- app/src/main/java/kr/hs/emirim/yejin/mirim_project_0614_3/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/yejin/mirim_project_0614_3/MainActivity.java
@@ -5,15 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
     ViewFlipper vFlip;
+    EditText editInterval;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         vFlip = findViewById(R.id.vflip);
+        editInterval = findViewById(R.id.edit_interval);
         Button btnStart = findViewById(R.id.btn_start);
         Button btnStop = findViewById(R.id.btn_stop);
         Button btnSceond = findViewById(R.id.btn_second);
@@ -33,6 +36,9 @@ public class MainActivity extends AppCompatActivity {
                     vFlip.stopFlipping();
                     break;
                 case R.id.btn_second:
+                    int second = Integer.parseInt(editInterval.getText().toString());
+                    vFlip.setFlipInterval(second*1000);
+                    break;
             }
         }
     };
